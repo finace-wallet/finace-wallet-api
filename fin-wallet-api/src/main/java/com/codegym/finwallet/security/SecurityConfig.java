3ff1efb5bc33091e3e6484f56776b360bfd15ffa
WIN-ASS03MD4SLB\Administrator <sn050920@gmail.com>
2024-04-17 10:15:57 +0700

    re:active security

--- fin-wallet-api/src/main/java/com/codegym/finwallet/security/SecurityConfig.java
+++ fin-wallet-api/src/main/java/com/codegym/finwallet/security/SecurityConfig.java
@@ -18,8 +18,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-//@EnableWebSecurity
-//@EnableMethodSecurity
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
