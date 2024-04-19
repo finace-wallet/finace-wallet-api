d1816c57af8b0edde4d9c4c3878f2063c1901948
WIN-ASS03MD4SLB\Administrator <sn050920@gmail.com>
2024-04-19 16:46:09 +0700

    add flyway in project

---
+++ fin-wallet-api/src/main/resources/db/migration/V1.0__CREATE_TABLE.sql
@@ -0,0 +1,23 @@
+CREATE TABLE app_user(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    password VARCHAR NOT NULL ,
+    email VARCHAR NOT NULL,
+    is_delete BOOLEAN DEFAULT false,
+    is_active BOOLEAN DEFAULT false
+);
+
+CREATE TABLE role(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    role_type VARCHAR NOT NULL,
+    is_delete BOOLEAN DEFAULT false
+);
+
+CREATE TABLE user_roles(
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id,role_id),
+    FOREIGN KEY (user_id) REFERENCES app_user(id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+CREATE TABLE 
