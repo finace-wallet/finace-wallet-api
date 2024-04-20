64e2a33dce3e5ad4f39ee7a6f994bf3b0ddc8393
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2024-04-20 20:19:04 +0700

    update+delete and conflict finished

--- fin-wallet-api/src/main/java/com/codegym/finwallet/service/WalletService.java
+++ fin-wallet-api/src/main/java/com/codegym/finwallet/service/WalletService.java
@@ -1,11 +1,20 @@
 package com.codegym.finwallet.service;
+
+
+
+import com.codegym.finwallet.dto.CommonResponse;
+import com.codegym.finwallet.dto.payload.request.WalletRequest;
 import com.codegym.finwallet.entity.AppUser;
 import com.codegym.finwallet.entity.Wallet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface WalletService extends GeneralService<Wallet> {
-    Page<Wallet> findAllByUser(Pageable pageable, AppUser appUser);
-    Iterable<Wallet> findAllByUser(AppUser appUser);
-    Wallet edit(Long id);
+import java.util.Optional;
+
+public interface WalletService {
+    Page<Wallet> findAllByEmail(Pageable pageable);
+    Wallet save(WalletRequest request);
+    void remove(Long id);
+    Wallet findById(Long id);
+    CommonResponse editWallet(Long id, WalletRequest walletRequest);
 }
--- src/main/java/com/codegym/finwallet/service/impl/WalletServiceImpl.java
+++ src/main/java/com/codegym/finwallet/service/impl/WalletServiceImpl.java
@@ -1,15 +1,19 @@
 package com.codegym.finwallet.service.impl;
+
+import com.codegym.finwallet.dto.CommonResponse;
 import com.codegym.finwallet.dto.payload.request.WalletRequest;
 import com.codegym.finwallet.entity.AppUser;
 import com.codegym.finwallet.entity.Wallet;
-import com.codegym.finwallet.repository.AppUserRepo;
+import com.codegym.finwallet.repository.AppUserRepository;
 import com.codegym.finwallet.repository.WalletRepository;
 import com.codegym.finwallet.service.WalletService;
+import com.google.api.Http;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -21,6 +25,7 @@ import java.util.Optional;
 public class WalletServiceImpl implements WalletService {
     private final WalletRepository walletRepository;
     private final ModelMapper modelMapper;
+    private final AppUserRepository appUserRepository;
 
     @Override
     public Page<Wallet> findAllByEmail(Pageable pageable) {
@@ -33,11 +38,6 @@ public class WalletServiceImpl implements WalletService {
     public Wallet save(WalletRequest request) {
         Wallet wallet = modelMapper.map(request,Wallet.class);
         return walletRepository.save(wallet);
-    }
-
-    @Override
-    public Wallet edit(Long id) {
-        return walletRepository.save(walletRepository.findById(id).get());
     }
 
     @Override
@@ -54,5 +54,34 @@ public class WalletServiceImpl implements WalletService {
         }
         return newWallet;
     }
+
+    @Override
+    public CommonResponse editWallet(Long id, WalletRequest walletRequest) {
+        try{
+            Wallet wallet = findById(id);
+            float curentAmount = wallet.getAmount();
+            float inputAmound = walletRequest.getAmount();
+            float newAmount = curentAmount + inputAmound;
+            wallet.setAmount(newAmount);
+            wallet.setIcon(wallet.getIcon());
+            wallet.setName(walletRequest.getName());
+            wallet.setDescription(walletRequest.getDescription());
+            walletRepository.save(wallet);
+
+            return CommonResponse.builder()
+                    .data(wallet)
+                    .message("Thành công!")
+                    .status(HttpStatus.OK)
+                    .build();
+        }catch (SecurityException e){
+            return CommonResponse.builder()
+                    .data(null)
+                    .message("Thất bại")
+                    .status(HttpStatus.BAD_REQUEST)
+                    .build();
+        }
+
+    }
+
 
 }
