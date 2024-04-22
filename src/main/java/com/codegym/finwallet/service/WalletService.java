fc2f4107f2e886adb7cb96174b6f1c89bf94a4b8
WIN-ASS03MD4SLB\Administrator <sn050920@gmail.com>
2024-04-22 12:39:06 +0700

    refactor code

--- src/main/java/com/codegym/finwallet/service/WalletService.java
+++ src/main/java/com/codegym/finwallet/service/WalletService.java
@@ -1,15 +1,11 @@
 package com.codegym.finwallet.service;
 
 
-
 import com.codegym.finwallet.dto.CommonResponse;
 import com.codegym.finwallet.dto.payload.request.WalletRequest;
-import com.codegym.finwallet.entity.AppUser;
 import com.codegym.finwallet.entity.Wallet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.Optional;
 
 public interface WalletService {
     Page<Wallet> findAllByEmail(Pageable pageable);
