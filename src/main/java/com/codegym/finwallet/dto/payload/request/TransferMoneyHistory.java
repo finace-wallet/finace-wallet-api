88f26e96a81bb161d06d3ea82e2b68be28dcc1a6
lelong199896 <lelong199896@gmail.com>
2024-04-25 14:05:24 +0700

    commit

---
+++ src/main/java/com/codegym/finwallet/dto/payload/request/TransferMoneyHistory.java
@@ -0,0 +1,16 @@
+package com.codegym.finwallet.dto.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class TransferMoneyHistory {
+    private String senderName;
+    private String ecipientName;
+    
+}
