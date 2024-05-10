15816b79efcf71bc6104053ba5f5e32bb3a5f2fc
WIN-ASS03MD4SLB\Administrator <sn050920@gmail.com>
2024-05-10 10:54:19 +0700

    get list transaction history in wallet

--- src/main/java/com/codegym/finwallet/repository/TransactionRepository.java
+++ src/main/java/com/codegym/finwallet/repository/TransactionRepository.java
@@ -1,7 +1,11 @@
 package com.codegym.finwallet.repository;
 
 import com.codegym.finwallet.entity.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    Page<Transaction> findByTransactionCategoryId(Long transactionCategoryId, Pageable pageable);
+    Page<Transaction> findAllByWalletId(Long walletId, Pageable pageable);
 }
--- src/main/java/com/codegym/finwallet/service/TransactionService.java
+++ src/main/java/com/codegym/finwallet/service/TransactionService.java
@@ -3,7 +3,11 @@ package com.codegym.finwallet.service;
 import com.codegym.finwallet.dto.CommonResponse;
 import com.codegym.finwallet.dto.payload.request.TransactionRequest;
 import com.codegym.finwallet.entity.Transaction;
+import com.codegym.finwallet.entity.TransactionCategory;
+import org.springframework.data.domain.Pageable;
 
 public interface TransactionService {
     CommonResponse saveTransaction(TransactionRequest request, Long walletId);
+    CommonResponse findAllTransactionsByWalletId(Long walletId, Pageable pageable);
+    CommonResponse findAllTransactionsByCategory(Long transactionCategoryId, Pageable pageable);
 }
